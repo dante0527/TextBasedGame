8c4a57c3bf8a7cb59dd2170bbcaa87e8366fa70e
Justin Swistak <jts7382@rit.edu>
2022-05-12 22:48:41 -0400

    fixed the prompts after doing action

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
 
+from PIL import Image                                                                                
+img = Image.open('ShadowGameMap.png')
+img.show() 
+
+
 # A dictionary for the simplified dragon text game
 # The dictionary links a room to other rooms
 rooms = {
@@ -12,6 +17,8 @@ rooms = {
         'Volcano': {'West': 'Bat Cavern', 'Item': 'Elderberry'},
         'Dojo': {'West': 'Bazaar', 'Boss': 'Shadowman'}
     }
+
+vowels = ['a', 'e', 'i', 'o', 'u']
 
 # Variable to track current room
 # Initialized with starting room
@@ -42,6 +49,7 @@ clear()
 
 # Gameplay loop
 while True:
+
     prompt()
     # Start-of-turn message
     print(f"\nYou are in the {current_room}\nInventory : {inventory}\n{'-' * 27}")
@@ -49,28 +57,44 @@ while True:
     # Tracks current room's item if there is one
     # If there is an item in current_room that isn't in your inventory, tell the user
     if "Item" in rooms[current_room].keys():
+
         nearby_item = rooms[current_room]["Item"]
+
         if nearby_item not in inventory:
-            print(f"You see a {nearby_item}")
+
+            if nearby_item[-1] == 's':
+                print(f"You see {nearby_item}")
+
+            elif nearby_item[1] in vowels:
+                print(f"You see an {nearby_item}")
+
+            else:
+                print(f"You see a {nearby_item}")
 
     if "Boss" in rooms[current_room].keys():
         print("You see a shadow of yourself.")
 
     # Accepts user input
     user_input = input('Enter your move:\n')
+
     # Splits move into words
     next_move = user_input.split(' ')
+
     # action determines what the user wants to do
     # object determines where the action is directed
     action = next_move[0]
+
     if len(next_move) > 1:
         object = next_move[1].title()
 
     if current_room == 'Dojo':
+
         if action == 'fight':
+
             if len(inventory) < 6:
                 print("You came unprepared. Your shadow consumes you.\nGAME OVER")
                 break
+
             else:
                 print("\nYou approach your shadow and offer him an altoid.\nIt accepts your offering and allows you to leave.\nYOU WIN")
                 break
@@ -78,10 +102,14 @@ while True:
     # If action is 'go'
     #   set current_room to room associated with object
     if action == 'go':
+
         try:
             current_room = rooms[current_room][object]
+            print(f"You travel {object}", end='')
+
         except:
             print("You can't go that way.")
+           
     # If action is 'get'
     #   if item is present
     #       if item is not in inventory
@@ -89,23 +117,29 @@ while True:
     elif action == 'get':
         try:
             if object in rooms[current_room].values():
+
                 if object not in inventory:
+
                     inventory.append(rooms[current_room]["Item"])
                     print(f"{object} retrieved!")
+
                 else:
                     print(f"You already have the {object}.")
+
             else:
                 print(f"Can't find {object}.")
+
         except:
             print("You can't get that item")
 
     # Exit command breaks while-loop and terminates program
     elif action == 'exit':
         break
+
     # If user_input is invalid, the user is notified and the while-loop continues
     else:
         print("Invalid command")
-
-    input("Press enter to try something else...")
+        
+    input()
     clear()
     
